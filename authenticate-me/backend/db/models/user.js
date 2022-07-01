1422ac03de25646d067b1cd04569f652faa9cf49
Andrew Parks <andrew.parks@tutanota.com>
2022-06-30 21:05:40 -0700

    phase 4 complete

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -61,6 +61,7 @@ module.exports = (sequelize, DataTypes) => {
       email: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
         validate: {
           len: [3, 256]
         }
